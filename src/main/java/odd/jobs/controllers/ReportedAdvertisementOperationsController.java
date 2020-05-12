f2bffec94beacbc4d4127ea3161e548599a03621
Patryk Margasinski <patrykmargasinski@gmail.com>
2020-05-12 18:29:18 +0200

    Added operations for ReportedAdvertisement

---
+++ src/main/java/odd/jobs/controllers/ReportedAdvertisementOperationsController.java
@@ -0,0 +1,49 @@
+package odd.jobs.controllers;
+
+import javassist.NotFoundException;
+import odd.jobs.entities.advertisement.ReportedAdvertisement;
+import odd.jobs.services.advertisement.ReportedAdvertisementOperationsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ReportedAdvertisementOperationsController {
+
+    private final ReportedAdvertisementOperationsService reportedAdvertisementService;
+
+    @Autowired
+    public ReportedAdvertisementOperationsController(ReportedAdvertisementOperationsService reportedAdvertisementService) {
+        this.reportedAdvertisementService = reportedAdvertisementService;
+    }
+
+    @GetMapping("/reported")
+    public List<ReportedAdvertisement> getAllReportedAdvertisement() {
+        return reportedAdvertisementService.getAllReportedAdvertisement();
+    }
+
+    @GetMapping("/reported/{id}")
+    public ReportedAdvertisement getReportedAdvertisement(@PathVariable long id) throws NotFoundException {
+        return reportedAdvertisementService.getReportedAdvertisement(id);
+    }
+
+    @PostMapping("/reported")
+    public void report(
+            @RequestParam(value="userId") long userId,
+            @RequestParam(value="advertisementId") long advertisementId,
+            @RequestParam(value="description") String description) throws NotFoundException {
+        reportedAdvertisementService.report(userId, advertisementId, description);
+    }
+
+    @PatchMapping("/reported/{id}")
+    public void acceptReport(@PathVariable long id) throws NotFoundException {
+        reportedAdvertisementService.acceptReport(id);
+    }
+
+    @DeleteMapping("/reported/{id}")
+    public void rejectReport(@PathVariable long id) throws NotFoundException {
+        reportedAdvertisementService.rejectReport(id);
+    }
+
+}
---
+++ src/main/java/odd/jobs/services/advertisement/ReportedAdvertisementOperationsService.java
@@ -0,0 +1,82 @@
+package odd.jobs.services.advertisement;
+
+import javassist.NotFoundException;
+import odd.jobs.entities.advertisement.Advertisement;
+import odd.jobs.entities.advertisement.AdvertisementCategory;
+import odd.jobs.entities.advertisement.ReportedAdvertisement;
+import odd.jobs.entities.user.User;
+import odd.jobs.repositories.AdvertisementRepository;
+import odd.jobs.repositories.ReportedAdvertisementRepository;
+import odd.jobs.repositories.UserRepository;
+import odd.jobs.services.advertisement.validator.SaveAdvertisementValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ReportedAdvertisementOperationsService {
+
+    private final ReportedAdvertisementRepository reportedAdvertisementRepository;
+    private final UserRepository userRepository;
+    private final AdvertisementRepository advertisementRepository;
+
+
+    @Autowired
+    public ReportedAdvertisementOperationsService(ReportedAdvertisementRepository reportedAdvertisementRepository, UserRepository userRepository, AdvertisementRepository advertisementRepository) {
+        this.reportedAdvertisementRepository = reportedAdvertisementRepository;
+        this.userRepository = userRepository;
+        this.advertisementRepository = advertisementRepository;
+    }
+
+    public List<ReportedAdvertisement> getAllReportedAdvertisement() {
+        List<ReportedAdvertisement> temp = new ArrayList<>();
+        reportedAdvertisementRepository.findAll().forEach(temp::add);
+        return temp;
+    }
+
+    public ReportedAdvertisement getReportedAdvertisement(Long id) throws NotFoundException {
+        List<ReportedAdvertisement> temp = new ArrayList<>();
+        ReportedAdvertisement reportedAdvertisement = reportedAdvertisementRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Reported advertisement not found"));
+        return reportedAdvertisement;
+    }
+
+    public void report(long userId, long advertisementId, String description) throws NotFoundException {
+        ReportedAdvertisement reportedAdvertisement=new ReportedAdvertisement();
+        Advertisement advertisement=advertisementRepository.findById(advertisementId)
+                .orElseThrow(()->new NotFoundException("Advertisement with id "+advertisementId+" not found"));
+        User user=userRepository.findById(userId)
+                .orElseThrow(()->new NotFoundException("User with id "+userId+" not found"));
+        reportedAdvertisement=reportedAdvertisement.toBuilder()
+                .advertisement(advertisement)
+                .user(user)
+                .description(description).build();
+        reportedAdvertisementRepository.save(reportedAdvertisement);
+    }
+
+    public void acceptReport(long id) throws NotFoundException {
+        ReportedAdvertisement reportedAdvertisement = reportedAdvertisementRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Reported advertisement not found"));
+        Advertisement advertisement = reportedAdvertisement.getAdvertisement();
+        reportedAdvertisementRepository.delete(reportedAdvertisement);
+        advertisementRepository.delete(advertisement);
+    }
+
+    public void rejectReport(long id) throws NotFoundException {
+        ReportedAdvertisement reportedAdvertisement = reportedAdvertisementRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Reported advertisement not found"));
+        reportedAdvertisementRepository.delete(reportedAdvertisement);
+    }
+}
